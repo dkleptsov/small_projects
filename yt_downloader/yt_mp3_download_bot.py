9e694a03589dafb653fe02ab54bab8b6d2816574
Denis Kleptsov <denis@kleptsov.com>
2020-12-07 21:06:46 +0300

    Added mp3 send back functionality
    Now bot send mp3 file back to user

--- yt_downloader/yt_mp3_download_bot.py
+++ yt_downloader/yt_mp3_download_bot.py
@@ -25,18 +25,23 @@ def main():
 
     @dp.message_handler()
     async def sum_message(message: types.Message):
+        # Download, save, encode and send
         start = time.time()
         video_info = youtube_dl.YoutubeDL().extract_info(url=message["text"], download=False)
         PARAMS['outtmpl'] = f"music/{video_info['title']}.mp3"
-
         with youtube_dl.YoutubeDL(PARAMS) as ydl:
             ydl.download([video_info['webpage_url']])
+        await bot.send_audio(message['from']['id'], audio=open(f"music/{video_info['title']}.mp3", 'rb'))
         response_time = f"{time.time() - start:.3f}"
         await message.answer(f"Response time: {response_time} sec")
-        log_entry = f"\nMessage from: {message['from']['username']} \nResponse time: {response_time} sec"
+
+        # Logging
+        log_entry = f"\nMessage from: {message['from']['id']} \nResponse time: {response_time} sec"
         logger.info(log_entry)
         await bot.send_message(91675683, log_entry)
-        gc.collect()  # Garbage collection
+
+        # Garbage collection
+        gc.collect()
 
     logger.info("Youtube downloader bot started!")
     try:
