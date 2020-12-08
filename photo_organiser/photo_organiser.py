b9c341ef05bb40c82bdd7c3aa2d22cb01a324c91
Denis Kleptsov <denis@kleptsov.com>
2020-12-08 17:05:25 +0300

    Update photo_organiser.py
    - Added info extraction from image files

--- photo_organiser/photo_organiser.py
+++ photo_organiser/photo_organiser.py
@@ -1,17 +1,43 @@
 import os
+import ntpath
+import hashlib
+from PIL import Image
+from PIL.ExifTags import TAGS
+from pathlib import Path
+from tqdm import tqdm
+ 
+
+def getinfo(image_path: str):
+    image_info = {}
+    image_info['path'] = image_path
+    image_info['file_name'] = ntpath.basename(image_path)
+    image_info['type'] = image_info['file_name'].split('.')[-1]
+    # Calculate hash
+    hasher = hashlib.md5()
+    with open(image_path, 'rb') as file:
+        data = file.read()
+        hasher.update(data)
+    image_info['hash'] = hasher.hexdigest()
+    # Get year of the image
+    try:
+        image = Image.open(image_path)
+        image_info['date'] = image.getexif()[36867]
+    except Exception:
+        image_info['date'] = None
+
+    return image_info
 
 
 def main():
-    # Itterate through subfolders and files in folder
-    directory = r'D:\OutlookDB\Foto2020'
-    for filename in os.listdir(directory):
-        if filename.endswith(".jpg") or filename.endswith(".png"):
-            print(os.path.join(directory, filename))
-        else:
-            continue
-    # Get information from each file
+    SOURCE_FOLDER = r'G:/PHOTO_SORTING/'
 
-    # Store information in database
+    # Itterate through subfolders and files in folder
+    paths = Path(SOURCE_FOLDER).glob('**/*')
+    for path in tqdm(paths):
+        if path.is_file():
+            getinfo(path)
+
+        # Store information in database
 
     # Organise and delete dublicates
 
