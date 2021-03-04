20e681835cc65392c53b373ea05fad3aceb3129d
Denis Kleptsov <denis@kleptsov.com>
2021-03-04 22:02:00 +0300

    Patterns for fizlica and other improvements

--- regex/multi_re.py
+++ regex/multi_re.py
@@ -2,26 +2,40 @@ import re
 import csv
 
 
-def check_nko(text_to_check:str, mode="normal", patterns_db="patterns_db.csv"):
-    # ! Возвращать детектированную часть с нормальной капитализацией (re.IGNORECASE или нарезать оригинальный текст)
-    # Детектирование форм одного слова
-    # Детектирование словосочетаний
-    # Детектирование нескольких словосочетаний одним паттерном
+def re_check(text_to_check:str, pattern:str, name:str, nko_type:str, org_type:str, date:str, excluded:str):
+    # ! Не делать текст lower, а использовать игноркейс  (re.IGNORECASE или нарезать оригинальный текст)
+    # ! Сокращенная и ласкательная форма имен Люда и т.д. Решение: сократить до части, которая не меняется Люд
+    # ! Надо ли ставить знак ? перед несколькими регулярками
+    # ? Детектирование словосочетаний
+    # ? Детектирование нескольких словосочетаний одним паттерном
+    # Детектирование организаций
+    # Придумать паттерны для extended режима
     lowered_text = text_to_check.lower()
+    compiled = re.compile(r'{}'.format(pattern, re.IGNORECASE))
+    matches = compiled.finditer(lowered_text)
+    result = {}
+    for i, match in enumerate(list(matches)):
+        print(i)
+        print(X)
+        # result [i] = {"span":list(match.span()), "text_found": text_to_check[match.start(): match.end()], 
+        #             "name": name, "nko_type": nko_type, "org_type": org_type, "date": date, "excluded": excluded}
+    
+    # return result
+    
+
+def check_nko(text_to_check:str, extended=False, patterns_db="patterns_db.csv"):
     with open(patterns_db) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=';')
         for row in csv_reader:
-            compiled = re.compile(r'{}'.format(row[1], re.IGNORECASE))
-            matches = compiled.finditer(lowered_text)
-            for match in matches:
-                result = {"span":list(match.span()), "text_found": match.group(),  # text_to_check[int(match.span()[0]), int(match.span()[1])], 
-                          "info": row[2] + row[3] + row[4] + row[5] + row[6]}
-                print(result)
-        
+            if extended:
+                return re_check(text_to_check, row[0], row[2], row[3], row[4], row[5], row[6])
+            else:
+                return re_check(text_to_check, row[1], row[2], row[3], row[4], row[5], row[6])
+                 
 
 def main():
     text_file = open("text_to_search.txt", "r", encoding="utf-8")
-    check_nko(text_file.read()) # all_nko)
+    print(check_nko(text_file.read()))
     text_file.close()
 
 
@@ -30,7 +44,6 @@ if __name__ == "__main__":
 
 
 ##### TO DO ######
-# Загрузка регулярок и доп информации из CSV
 # Сделать правила для физлиц
 # Сделать правила для юрлиц
 # Очистка текста от мусора? (удалять все не буквы? А как возвращать результаты? Есть ли цифры?)
