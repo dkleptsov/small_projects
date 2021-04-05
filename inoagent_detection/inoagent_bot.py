4fe822b96bcd163aaf81b626627c68a9603459fd
Denis Kleptsov <denis@kleptsov.com>
2021-04-05 13:22:40 +0200

    Refactored to run on Linux

--- inoagent_detection/inoagent_bot.py
+++ inoagent_detection/inoagent_bot.py
@@ -1,46 +1,68 @@
+#!/usr/bin/python3
 import os
 import sys
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types.message import ContentType
 import time
 from loguru import logger
 from test_pattern import check_all_patterns
 import gc
 
 LOGS_PATH = r"logs/inoagent_bot.log"
-BOT_TOKEN = os.getenv("INOAGENT_BOT")
+ADMIN_NICK = "real_den"
+BOT_TOKEN = os.getenv("INOAGENT_BOT") #os.getenv("TESTFLIGHT_BOT") #
 START_MSG = "Этот бот проверяет текст на наличие упоминаний организаций, \
 которые признаны иностранными агентами в Российской Федерации. 🕵️"
-CONTACTS_MSG = "По всем вопросам пишите @real_den"
+CONTACTS_MSG = f"По всем вопросам пишите @{ADMIN_NICK}"
 AWAIT_MSG = "Ваше сообщение получено, пожалуста, ожидайте ответа. 🤗"
+NO_RES_MSG = f"Мы не нашли в тексте вашего сообщения упоминаний \
+иностранных агентов 🤔. Если вам кажется, что мы что-то пропустили, \
+пожалуйста, напишите об этом @{ADMIN_NICK}"
+UNKN_CONTENT = "Этот бот умеет работать только с текстами. 😌"
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+keyboard_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+btns_text = ('💡 Как это работает', '🤓 Контакты')
+keyboard_markup.row(*(types.KeyboardButton(text) for text in btns_text))
+
+
+async def set_commands(bot: Bot):
+    commands = [
+        BotCommand(command="/start", description="Запустить бота."),
+        BotCommand(command="/help", description="Свяжитесь с нами."),
+    ]
+    await bot.set_my_commands(commands)
 
 
 @logger.catch
 def main():
     logger.add(LOGS_PATH, format="{time} {level} {message}", retention="14 days"
                , serialize=True)
-    bot = Bot(token=BOT_TOKEN)
-    dp = Dispatcher(bot)
-
-    keyboard_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btns_text = ('💡 Как это работает', '🤓 Контакты')
-    keyboard_markup.row(*(types.KeyboardButton(text) for text in btns_text))
 
     @dp.message_handler(commands=['start'])
     async def start_message(message: types.Message):
         await message.reply(START_MSG, reply_markup=keyboard_markup)
+    
+    @dp.message_handler(commands=['help'])
+    async def start_message(message: types.Message):
+        await message.reply(CONTACTS_MSG, reply_markup=keyboard_markup)
+
+    @dp.message_handler(lambda message: message["text"] =="💡 Как это работает")
+    async def how_it_works(message: types.Message):
+        await message.answer(START_MSG, reply_markup=keyboard_markup)
+
+    @dp.message_handler(lambda message: message["text"] == "🤓 Контакты")
+    async def contact_us(message: types.Message):
+        await message.answer(CONTACTS_MSG, reply_markup=keyboard_markup)
 
     @dp.message_handler()
     async def sum_message(message: types.Message):
-        if message["text"] == '💡 Как это работает':
-            await message.answer(START_MSG, reply_markup=keyboard_markup)
-        elif message["text"] == '🤓 Контакты':
-            await message.answer(CONTACTS_MSG, reply_markup=keyboard_markup)
-        else:
-            await message.answer(AWAIT_MSG, reply_markup=keyboard_markup)
-
+        await message.answer(AWAIT_MSG, reply_markup=keyboard_markup)
         start = time.time()
         results = check_all_patterns(message["text"], 
-        patterns_db=r"D:/small_projects/inoagent_detection/patterns_db.csv")
+        patterns_db=r"/home/small_projects/inoagent_detection/patterns_db.csv")
+        #r"D:/small_projects/inoagent_detection/patterns_db.csv")
         if len(results) > 0:
             for i in range(len(results)):
                 await message.answer(
@@ -51,6 +73,10 @@ def main():
                     \nДата включения в реестр: {results[i]['date']}")
             await message.answer(
                 f"Время обработки: {time.time() - start:.3f} секунд")
+        else:
+            await message.answer(NO_RES_MSG + 
+            f"\nВремя обработки: {time.time() - start:.3f} секунд")
+
         # Logging
         log_entry = f"\nMessage from: {message['from']['id']}\
                       \nText: {message['text']}\
@@ -60,6 +86,10 @@ def main():
 
         # Garbage collection
         gc.collect()
+    
+    @dp.message_handler(content_types=ContentType.ANY)
+    async def unknown_content(message: types.Message):
+        await message.reply(UNKN_CONTENT)
 
     logger.info("Inoagent bot started!")
     try:
