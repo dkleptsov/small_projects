6301b3291b8a3b5009961cbd0e3d8c1f48369a69
dkleptsov <47938353+dkleptsov@users.noreply.github.com>
2021-05-28 09:56:28 +0300

    Launched and it works!

--- web_scraping/aaic.py
+++ web_scraping/aaic.py
@@ -1,16 +1,44 @@
-from selenium import webdriver
 from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox()
+MESSAGE = 'Hello! How are you doing?\
+\nCurrently I am studying Deep learning and I really want to apply my knowledge to real life projects. \
+\nPlease let me know If you have any projects where we might use Artificial Intelligence (AI).\
+\nI have experience in Natural language processing and Computer vision.\
+\nYou can find full list of my certificates on my LinkeIn page: https://www.linkedin.com/in/kleptsov/ \
+\nI can work as an employee or as a contractor.\
+\nIf you wish, we can arrange a video conference (e.g. "Zoom") outside of this conference to discuss all details and possibilities.\
+\nAlso I can create web and telegram bots to automate different tasks.\
+\nThanks in advance. I look forward to hearing from you. Kind regards.\
+\nDenis Kleptsov'
 
+driver = webdriver.Chrome()
 driver.get("https://aaic2021.b2match.io/login")
-for i in range (141, 142):
+
+# Логинимся
+driver.find_element_by_id('Email').send_keys('denis@kleptsov.com')
+driver.find_element_by_id('Password').send_keys('busumsp1')
+driver.find_element_by_id('Password').send_keys(Keys.ENTER)
+
+for i in range (200, 1500):
     aaic_url = f"https://aaic2021.b2match.io/participants/{i}"
     driver.get(aaic_url)
-    sleep(100)
+    sleep(1)
+    try:
+        driver.find_element_by_xpath("/html/body/section/main/section/div/div[1]/div/div[3]/a").click()
+        sleep(1)
+        driver.find_element_by_xpath("/html/body/section/main/div[2]/div/div/div[2]/form/div/div/textarea").send_keys(MESSAGE)
+        driver.find_element_by_xpath("/html/body/section/main/div[2]/div/div/div[2]/form/div/div/div/button[2]").click()
+        sleep(2)
+    except:
+        sleep(1)
+
+
+    sleep(2)
 
 
 
 
-sleep(5)
+sleep(500)
 driver.quit()
